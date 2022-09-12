a875586f036fe8807c55fac65c66ae0a5e03414e
Scott Rinaberger <scottrinaberger@gmail.com>
2022-09-12 18:57:18 -0500

    add first round of questions

--- index.js
+++ index.js
@@ -1,2 +1,96 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const genReadme = require('./utils/generateMarkdown.js');
+
+
+// TODO: Create an array of questions for user input
+const promptQuestions = readmeData => {
+    console.log(`
+    ================
+    Add A New Readme
+    ================
+    `);
+  
+    return inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'projectTitle',
+                message: 'What is the name your project? (Required)',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('You need to enter a project name.');
+                        return false;
+                    }
+                }            
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: 'Describe your project. (Required)',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('You need to enter a project description.');
+                        return false;
+                    }
+                }            
+            },
+            {
+                type: 'input',
+                name: 'install',
+                message: 'Describe how to install your code. (Required)',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('You need to enter a project description.');
+                        return false;
+                    }
+                }            
+            },
+            {
+                type: 'input',
+                name: 'usage',
+                message: 'Describe how to install your code. (Required)',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('You need to enter a project description.');
+                        return false;
+                    }
+                }            
+            },
+            {
+                type: 'input',
+                name: 'testInstruct',
+                message: 'Describe how test your code. (Required)',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('You need to enter a project description.');
+                        return false;
+                    }
+                }            
+            },
+
+        ])
+}
+
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
+
+promptQuestions()
+  //.then(promptQuestions)
 
