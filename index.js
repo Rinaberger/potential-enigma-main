672badbfbcc7cd0ad0c87c523668d83be113b128
Scott Rinaberger <scottrinaberger@gmail.com>
2022-09-20 14:03:33 -0500

    final questions

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
 const genReadme = require('./utils/generateMarkdown.js');
+const { writeFile, copyFile } = require('./utils/generateMarkdown.js');
 
 
 // TODO: Create an array of questions for user input
@@ -41,12 +42,12 @@ const promptQuestions = readmeData => {
             {
                 type: 'input',
                 name: 'install',
-                message: 'Describe how to install your code. (Required)',
+                message: 'What are steps to install your project? (Required)',
                 validate: nameInput => {
                     if (nameInput) {
                         return true;
                     } else {
-                        console.log('You need to enter a project description.');
+                        console.log('You need to enter a install steps.');
                         return false;
                     }
                 }            
@@ -54,12 +55,25 @@ const promptQuestions = readmeData => {
             {
                 type: 'input',
                 name: 'usage',
-                message: 'Describe how to install your code. (Required)',
+                message: 'Provide examples for use. (Required)',
                 validate: nameInput => {
                     if (nameInput) {
                         return true;
                     } else {
-                        console.log('You need to enter a project description.');
+                        console.log('You need to enter examples of usage.');
+                        return false;
+                    }
+                }            
+            },
+            {
+                type: 'input',
+                name: 'contribute',
+                message: 'List any contributors or other credits. (Required)',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('You need to enter a project credits.');
                         return false;
                     }
                 }            
@@ -77,9 +91,8 @@ const promptQuestions = readmeData => {
                     }
                 }            
             },
-
         ])
-}
+};
 
 
 // // TODO: Create a function to write README file
@@ -92,5 +105,20 @@ const promptQuestions = readmeData => {
 // init();
 
 promptQuestions()
-  //.then(promptQuestions)
+  .then(readmeData => {
+    return genReadme(readmeData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });  //.then(promptQuestions)
 
